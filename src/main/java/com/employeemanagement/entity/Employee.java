f04ab59305eef41bed79dffccd760b974d9cbb69
rafal1018 <rafaljaneczek@o2.pl>
2021-08-21 19:43:18 +0200

    Create Employee entity

---
+++ src/main/java/com/employeemanagement/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.employeemanagement.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private EmploymentDetails employmentDetails;
+
+}
